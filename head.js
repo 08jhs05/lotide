86f6e5536b2aca08258b0d6f3b81850682a299e0
jayShin <08jhs05@gmail.com>
2021-03-29 20:27:21 +0000

    head function

---
+++ head.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function head(arr){
+  if(arr.length === 0){
+    return undefined;
+  } else return arr[0];
+}
