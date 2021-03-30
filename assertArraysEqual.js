fe77a62ac8d85195179b62368341dde7b98a5412
jayShin <08jhs05@gmail.com>
2021-03-30 21:13:53 +0000

    assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const eqArrays = function(arr1, arr2){
+  if(arr1.length !== arr2.length) return false;
+
+  let arraylength = arr1.length;
+  for(let i = 0; i < arraylength; i++){
+    if (arr1[i] !== arr2[i]) return false;
+  }
+
+  return true;
+}
+
+const assertArraysEqual = function(arr1, arr2){
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+}
