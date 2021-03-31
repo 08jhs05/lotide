d2f4fc8ff519d6b445bfdcfee23c9b1c98636d52
jayShin <08jhs05@gmail.com>
2021-03-31 21:00:47 +0000

    working on it

---
+++ letterPositions.js
@@ -0,0 +1,13 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  
+  for(let i = 0; i<sentence.length; i++){
+    if(sentence[i] !== " "){
+      if(results[sentence[i]] === undefined) results[sentence[i]] = [i];
+      else results[sentence[i]].push(i);
+    }
+  }
+  return results;
+};
+
+console.log(letterPositions("lighthouse lighthouse random thing"));
