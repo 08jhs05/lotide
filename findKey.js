2e5b33f69444c5198cb40a1b94762441ccf81cbb
jayShin <08jhs05@gmail.com>
2021-04-01 17:29:25 +0000

    findkey function

---
+++ findKey.js
@@ -0,0 +1,17 @@
+const findKey = function(obj, callback){
+  for(property in obj){
+    if(callback(obj[property])) return property;
+  }
+  return undefined;
+}
+
+const test = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)
+
+console.log(test);
